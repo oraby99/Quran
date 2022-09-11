bd8db2fddf9235be8bae7034f48f83a43d8b10da
mohammedahmed18 <mohammed18200118@gmail.com>
2022-09-11 16:47:15 +0200

    add meta route

--- src/server/routes/quran.js
+++ src/server/routes/quran.js
@@ -2,7 +2,10 @@ const { http } = require("../utils/http");
 const router = require("express").Router();
 
 // get all surahs
-router.get("/", (req, res) => {
+router.get("/meta", (req, res) => {
+    http('/meta').then(response => {
+        res.json(response.data)
+    })
 });
 
 module.exports = router;
